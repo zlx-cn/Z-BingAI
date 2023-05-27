610f5eca6ce39ec34ec92ccb7c6a31000e388546
zlx-cn <122418130+zlx-cn@users.noreply.github.com>
2023-05-27 12:10:25 +0800

    Update ChatNav.vue

--- frontend/src/components/ChatNav/ChatNav.vue
+++ frontend/src/components/ChatNav/ChatNav.vue
@@ -36,8 +36,8 @@ const navType = {
 const navConfigs = [
   {
     key: navType.github,
-    label: '开源地址',
-    url: 'https://github.com/adams549659584/go-proxy-bingai',
+    label: '设置：',
+    url: '#',
   },
   {
     key: navType.version,
